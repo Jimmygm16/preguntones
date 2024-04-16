fb6563385b8ec585ff9eaacf7ff5bd7b1c9b2e17
jimmygm16 <jimmya.giraldom@autonoma.edu.co>
2024-04-16 14:20:32 -0500

    landing, timer, setup players, game header

---
+++ src/views/Game.vue
@@ -0,0 +1,16 @@
+<script setup>
+import GameHeader from '@/components/GameHeader.vue'
+</script>
+
+<template>
+  <GameHeader />
+  <main class="page-content"></main>
+</template>
+
+<style scoped>
+.page-content {
+  width: 100%;
+  height: 100vh;
+  background-color: wheat;
+}
+</style>
