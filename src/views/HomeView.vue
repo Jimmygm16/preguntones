023dd098575a03b402a8acdcda3372db680a925d
jimmygm16 <jimmya.giraldom@autonoma.edu.co>
2024-04-15 21:04:47 -0500

    home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,64 @@
 <script setup></script>
 
 <template>
-  <h1>sadsd</h1>
+  <main class="main-content">
+    <div class="title-container">
+      <span class="title">Preguntones</span>
+    </div>
+    <div class="menu">
+      <div class="card">Individual</div>
+      <div class="card">Cooperativo local</div>
+      <div class="card">Configuración</div>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-image: url('src/assets/imgs/Designer.jpeg');
+  background-size: cover;
+}
+
+.title-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0rem 1.5rem;
+  border-bottom: solid 0.3rem var(--red);
+  backdrop-filter: blur(0.4rem);
+}
+
+.title {
+  color: var(--red);
+  font-size: 3rem;
+  font-weight: 900;
+  text-transform: uppercase;
+}
+
+.menu {
+  display: flex;
+  flex-direction: column;
+  padding-top: 3rem;
+  gap: 1.5rem;
+}
+
+.card {
+  display: flex;
+  border: solid 0.1rem white;
+  padding: 1rem 6rem;
+  justify-content: center;
+  background-color: wheat;
+  box-shadow: 0.1rem 0.1rem 0.1rem 0.1rem rgb(0, 0, 0, 0.4);
+}
+
+.card:hover {
+  cursor: pointer;
+  background-color: var(--blue);
+}
+</style>
