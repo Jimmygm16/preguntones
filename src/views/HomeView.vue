360b9cbb20ff8cf7647a78aad397e9fd4ca1cafb
PabloGoProg <pgosorio13@gmail.com>
2024-04-16 14:14:06 -0500

    Question v1 and SpinWheel v1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,45 @@
-<script setup></script>
+<script setup>
+import SpinWheel from '@/components/SpinWheel.vue'
+import { ref } from 'vue'
+
+const questionCategories = ref([])
+const loading = ref(true)
+
+const selectRandomCategories = async () => {
+  const getCategroies = async () => {
+    const response = await fetch('https://opentdb.com/api_category.php')
+    const data = await response.json()
+    questionCategories.value = data.trivia_categories
+  }
+
+  await getCategroies()
+  const randomCategories = []
+
+  for (let i = 0; i < 8; i++) {
+    const randomIndex = Math.floor(Math.random() * questionCategories.value.length)
+    randomCategories.push(questionCategories.value[randomIndex])
+  }
+
+  questionCategories.value = randomCategories
+  loading.value = false
+}
+
+selectRandomCategories()
+
+// const fetchQuestions = async () => {
+//   const response = await fetch('https://opentdb.com/api.php?amount=10')
+//   const data = await response.json()
+//   qs.value = data.results
+// }
+
+// fetchQuestions()
+</script>
 
 <template>
-  <h1>sadsds</h1>
+  <section v-if="loading">Cargando</section>
+  <section v-else>
+    <SpinWheel :questionTypes="questionCategories" />
+  </section>
 </template>
 
 <style scoped></style>
